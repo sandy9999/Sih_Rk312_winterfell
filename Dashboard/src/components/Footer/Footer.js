2a64d8431d2597e66720fd7d1ef0c8b507223e6b
shakeabi <abishake.110899@gmail.com>
2020-08-02 03:04:04 +0530

    Feat: Integrate map with dash

--- Dashboard/src/components/Footer/Footer.js
+++ Dashboard/src/components/Footer/Footer.js
@@ -30,25 +30,18 @@ class Footer extends React.Component {
         <Container fluid>
           <Nav>
             <NavItem>
-              <NavLink href="https://www.creative-tim.com/?ref=bdr-user-archive-footer">Creative Tim</NavLink>
+              <NavLink href="#">Winter is Coming</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://www.creative-tim.com/presentation?ref=bdr-user-archive-footer">About Us</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://www.creative-tim.com/blog?ref=bdr-user-archive-footer">Blog</NavLink>
+              <NavLink href="#">About Us</NavLink>
             </NavItem>
           </Nav>
           <div className="copyright">
             © {new Date().getFullYear()} made with{" "}
             <i className="tim-icons icon-heart-2" /> by{" "}
-            <a
-              href="https://www.creative-tim.com/?ref=bdr-user-archive-footer"
-              target="_blank"
-            >
-              Creative Tim
-            </a>{" "}
-            for a better web.
+            <a href="https://delta.nitt.edu" target="_blank">
+              Winterfell
+            </a>
           </div>
         </Container>
       </footer>
